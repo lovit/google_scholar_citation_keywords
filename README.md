ecad7f44a43da32eced2315aa6d285340001a5c6
lovit <soy.lovit@gmail.com>
2018-04-25 04:39:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Google scholar citation keywords
+
+Google scholar 에서 관심있는 논문을 citation 한 논문들의 주제를 요약하기 위한 작업
+
+관심있는 논문을 인용한 논문들의 제목, snippest 등을 스크래핑 한 뒤, 이를 바탕으로 키워드 분석을 수행
+
+
+## Scripts
+
+### Scrapping
+
+    python scrapper.py --input_file input.txt --sleep 5 --error_sleep 300 --output_suffix tsne
+
+or default
+
+    python scrapper.py
+
+
+| arguments | default | description |
+| --- | --- | --- |
+| input_file | input.txt | Input file. It contains front url and citation numbers |
+| sleep | 5 | Sleep time when a page was scrapped. Preventing Google's block |
+| error_sleep | 300 | Sleep time when exception occurs |
+| output_suffix | '' | Output file suffix. Output file prefix is scrapping start datetime |
+
+### Input file format. 
+
+아래 이미지처럼 찾고 싶은 논문의 citation 을 눌렀을 때 나오는 첫 화면. 아래 화면을 누르면 다음 url 을 얻을 수 있음
+
+    https://scholar.google.co.kr/scholar?cites=14270678001885118036&as_sdt=2005&sciodt=0,5&hl=en
+
+![](./images/front_url_snapshot.png)
+
+num citations 는 해당 논문이 인용된 횟수
+
+    4434
+
+### Output file format
+
+Tap separated. (title, authors, year, snippest, n_citations)
---
+++ scrapper.py
@@ -0,0 +1,149 @@
+import argparse
+import re
+import requests
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+year_pattern = re.compile('[\d]{4}')
+citation_pattern = re.compile('Cited by [\d]+')
+debug = False
+
+def clean_text(text):
+    return text.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
+
+def get_now_idx():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+def get_soup(url):
+    try:
+        r = requests.get(url)
+        html = r.text
+        page = BeautifulSoup(html, 'lxml')
+        return page
+    except Exception as e:
+        print(e)
+        return None
+
+def parse_title(div):
+    try:
+        return div.select('h3[class=gs_rt]')[0].text
+    except Exception as e:
+        if debug:
+            print(e)
+        return ''
+
+def parse_authors(div):
+    try:
+        # (user idx, name)
+        authors = div.select('div[class=gs_a] a')
+        authors = [(author.attrs.get('href', 'user=').split('user=')[1].split('&')[0], 
+                    author.text)
+                   for author in authors]
+        return authors
+    except Exception as e:
+        if debug:
+            print(e)
+        return []
+
+def parse_year(div):
+    year_pattern = re.compile('[\d]{4}')
+    try:
+        return year_pattern.findall(
+            div.select('div[class=gs_a]')[0].text)[0]
+    except Exception as e:
+        if debug:
+            print(e)
+        return ''
+
+def parse_snippest(div):
+    try:
+        return div.select('div[class=gs_rs]')[0].text
+    except Exception as e:
+        if debug:
+            print(e)
+        return ''
+
+def parse_num_of_citation(div):
+    citation_pattern = re.compile('Cited by [\d]+')
+    try:
+        return citation_pattern.findall(
+            div.select('div[class=gs_fl]')[0].text)[0][8:].strip()
+    except Exception as e:
+        if debug:
+            print(e)
+        return ''
+
+def load_input_file(path):
+    # url, num citation. Tap separated
+    with open(path, encoding='utf-8') as f:
+        docs = [line.strip().split('\t') for line in f]
+    docs = [(doc[0], int(doc[1])) for doc in docs if len(doc) == 2]
+    urls, num_citations = zip(*docs)
+    return urls, num_citations
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_suffix', type=str, default='',
+                        help='suffix attached after datetime idx. eg: 2018-04-28_03-45-23[suffix]')
+    parser.add_argument('--input_file', type=str, default='input.txt',
+                        help='file path that contains front urls')
+    parser.add_argument('--sleep', type=int, default=5,
+                        help='sleep time [sec] after one page scrapping was done')
+    parser.add_argument('--error_sleep', type=int, default=300,
+                        help='sleep time [sec] after exception occurs')
+    
+    args = parser.parse_args()
+    output_suffix = args.output_suffix
+    if output_suffix:
+        output_suffix = '-' + output_suffix
+    input_file = args.input_file
+    sleep = args.sleep
+    error_sleep = args.error_sleep
+
+    urls, num_citations = load_input_file(input_file)
+    
+    for front_url, num_citation in zip(urls, num_citations):
+        print('front url = {}'.format(front_url))
+        print('num citation = {}'.format(num_citation))
+
+        output_idx = get_now_idx() + output_suffix
+
+        last_start_index = int(num_citation/10)
+        for start in range(0, last_start_index + 1, 10):
+            print('begin with start={}'.format(start), flush=True)
+
+            url = (front_url + '&start={}'.format(start))
+            page = get_soup(url)
+            divs = page.select('div[class=gs_ri]')
+            if not divs or len(divs) == 0:
+                print('Something wrong. Sleep {} secs'.format(error_sleep))
+                time.sleep(error_sleep)
+                continue
+            
+            infos = []
+            for div in divs:
+                title = parse_title(div)
+                authors = parse_authors(div)
+                year = parse_year(div)
+                snippest = parse_snippest(div)
+                n_citations = parse_num_of_citation(div)
+                infos.append((clean_text(title),
+                              ' & '.join(['{}//{}'.format(idx, name) for idx, name in authors]),
+                              year,
+                              clean_text(snippest),
+                              n_citations))
+            
+            f = open('{}{}.txt'.format(output_idx, output_suffix), 'a', encoding='utf-8')
+            for info in infos:
+                f.write('{}\n'.format('\t'.join(info)))
+            f.close()
+
+            print('done with start={}'.format(start), flush=True)
+
+            if start >= 30: # debug code
+                break
+
+if __name__ == '__main__':
+    main()
